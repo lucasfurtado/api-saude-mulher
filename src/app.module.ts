3d22765b898d0580d64a729456feb8b9daa5bbec
Lucas Furtado <lucasfurtadodev@gmail.com>
2023-05-06 18:20:58 -0300

    Configuração com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OlaModule } from './ola/ola.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './config/database.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports:[OlaModule]
+    imports:[
+        OlaModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            useClass: DatabaseConfigService,
+            inject: [DatabaseConfigService]
+        }),
+    ]
 })
 export class AppModule {}
