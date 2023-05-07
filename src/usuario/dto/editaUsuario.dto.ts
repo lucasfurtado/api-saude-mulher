234cd741b6d2704f0e491715c21d24eb068dd53b
Lucas Furtado <lucasfurtadodev@gmail.com>
2023-05-06 21:43:37 -0300

    Colocando validações síncronas

--- src/usuario/dto/editaUsuario.dto.ts
+++ src/usuario/dto/editaUsuario.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
 export class EditaUsuarioDTO{
+    
+    @IsNotEmpty({message: 'O nome não pode ser vazio'})
     nome: string;
+
+    @IsEmail(undefined, {message: 'O email informado é invalido'})
     email: string;
+
+    @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres'})
     senha: string;
 }
--- src/usuario/dto/listaUsuarios.dto.ts
+++ src/usuario/dto/listaUsuarios.dto.ts
@@ -1,10 +1,4 @@
 export class ListaUsuariosDTO{
 
-    constructor(nome: string, email: string){
-        this.nome = nome;
-        this.email = email;
-    }
-
-    nome: string;
-    email: string;
+    constructor(readonly nome: string, readonly email: string){}
 }
